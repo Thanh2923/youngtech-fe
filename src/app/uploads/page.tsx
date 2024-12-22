e2438cb2f62c2435efad48b40a817be10c8311f7
Thanhnv2923 <thanhnv2923@gmail.com>
2024-12-22 16:23:14 +0700

    first commit

---
+++ src/app/uploads/page.tsx
@@ -0,0 +1,12 @@
+import UploadImage from '@/components/UploadImage'
+import React from 'react'
+
+const page = () => {
+  return (
+   <UploadImage handleGetArrayImage={function (urls: { url: string; public_id: string }[]): void {
+      throw new Error('Function not implemented.')
+    } }/>
+  )
+}
+
+export default page
